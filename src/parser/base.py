45b44e5f78b22eddde0820bbb58574b7112cd495
glebfel <felyust@list.ru>
2023-05-23 20:06:12 +0300

    modify base request method

--- src/parser/base.py
+++ src/parser/base.py
@@ -1,4 +1,6 @@
 import json
+from typing import Any
+from enum import StrEnum
 
 import aiohttp as aiohttp
 
@@ -15,12 +17,22 @@ class BaseThirdPartyAPIClient:
     headers = {}
     base_url = ''
 
-    async def request(self, edge: str, is_json: bool = True,
-                      proxy: str = None) -> str:
+    class HTTPMethods(StrEnum):
+        GET = 'GET'
+        POST = 'POST'
+        PUT = 'PUT'
+        PATCH = 'PATCH'
+        DELETE = 'DELETE'
+
+    async def request(self, method: HTTPMethods, edge: str, is_json: bool = True,
+                      proxy: str = None, querystring: dict = None, payload: Any = None) -> str:
         async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.get(
-                    '/'.join((self.base_url, edge)),
+            async with session.request(
+                    method=method.value,
+                    url='/'.join((self.base_url, edge)),
                     proxy=proxy,
+                    params=querystring,
+                    json=payload,
             ) as res:
                 return await self._clean_response(res, is_json=is_json)
 
