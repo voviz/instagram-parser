22d036ad658b2f387b43e9726483733749e4fed3
glebfel <felyust@list.ru>
2023-05-23 21:12:40 +0300

    update db models

---
+++ src/db/connector.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+
+from tortoise import Tortoise
+
+from src.core.settings import settings
+
+log = logging.getLogger(__name__)
+
+
+class DatabaseConnector:
+    def __init__(self):
+        self._sync_init()
+
+    async def _async_init(self):
+        await Tortoise.init(
+            db_url=f'asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@'
+                   f'{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}',
+            modules={'models': ['db.models']},
+        )
+        # Generate the schema
+        # safe=True - generate schema if not exists in db
+        await Tortoise.generate_schemas(safe=True)
+
+    def _sync_init(self):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self._async_init())
+
+    def get_proxies_list(self) -> list[str]:
+        pass
+
+
+database_connector = DatabaseConnector()
---
+++ src/db/models.py
@@ -0,0 +1,51 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class IdMixin:
+    id = fields.IntField(pk=True)
+
+
+class TimestampMixin:
+    created_at = fields.DatetimeField(null=True)
+    updated_at = fields.DatetimeField(null=True)
+
+
+class InstagramAccounts(Model, IdMixin):
+    credentials = fields.CharField(max_length=255, unique=True)
+    cookies = fields.CharField(max_length=5000, null=True)
+    user_agent = fields.BigIntField(max_length=255, null=True)
+    banned = fields.BooleanField(default=False)
+    last_used_at = fields.DatetimeField(null=True)
+
+    class Meta:
+        table = 'instagram_accounts'
+
+
+class InstagramLogins(Model, IdMixin, TimestampMixin):
+    username = fields.CharField(max_length=255, unique=True)
+    followers = fields.IntField(null=True)
+    is_exists = fields.BooleanField(null=True)
+
+    class Meta:
+        table = 'instagram_logins'
+
+
+class Proxies(Model, IdMixin):
+    proxy = fields.CharField(max_length=255, unique=True)
+    last_used_at = fields.DatetimeField(null=True)
+
+    class Meta:
+        table = 'proxies'
+
+
+class ParserResult(Model, TimestampMixin, IdMixin):
+    instagram_username = fields.CharField(max_length=255, null=True)
+    marketplace = fields.CharField(max_length=255, null=True)
+    story_publication_date = fields.DatetimeField(null=True)
+    articul = fields.CharField(max_length=255, null=True)
+    ad_type = fields.CharField(max_length=255, null=True)
+    is_checked = fields.BooleanField(default=False)
+
+    class Meta:
+        table = 'parser_result'
