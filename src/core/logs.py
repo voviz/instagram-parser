a1141c2528bbd4a236289602dd2bd17796823974
glebfel <felyust@list.ru>
2023-05-23 21:12:07 +0300

    add logging to project

---
+++ src/core/logs.py
@@ -0,0 +1,12 @@
+import pathlib
+import sys
+
+from loguru import logger
+
+# root directory
+ROOT_PATH = str(pathlib.Path(__file__).parent.parent)
+
+logger.remove(0)
+logger.add(sys.stderr, colorize=True)
+logger.add(ROOT_PATH + '/logs.log', level='INFO', rotation='100 MB', compression='zip', mode='a')
+custom_logger = logger.bind(specific=True)
