b3e9f67b0b420a0a3200df9b16b08ac2695216d6
glebfel <felyust@list.ru>
2023-05-24 22:23:09 +0300

    add base third-party api models

---
+++ src/parser/models.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, Field
+from enum import StrEnum, IntEnum
+
+
+class ThirdPartyAPISource(StrEnum):
+    """
+    Defines available media types from third party APIs.
+    """
+    instagram = 'instagram'
+    ozon = 'ozon'
+    wildberries = 'wildberries'
+
+
+class ThirdPartyAPIMediaType(IntEnum):
+    """
+    Defines available media types from third party APIs.
+    """
+    unknown = 0
+    photo = 1
+    video = 2
+    audio = 3
+    gif = 4
+
+
+class InstagramStory(BaseModel):
+    media_type: ThirdPartyAPIMediaType = Field(default=ThirdPartyAPIMediaType.unknown)
+    url: str
+
+
+class ThirdPartyAPIClientAnswer(BaseModel):
+    source: ThirdPartyAPISource
+
+
+class InstagramClientAnswer(ThirdPartyAPIClientAnswer):
+    """
+    Response from Instagram API.
+    """
+    user_id: int = Field(default=None)
+    username: str
+    stories_list: list[InstagramStory] = Field(default_factory=list)
