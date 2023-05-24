dc57a4cd8d983c5413d0840c13c2b1a7e909cede
glebfel <felyust@list.ru>
2023-05-24 22:23:24 +0300

    add get_account_stories_by_id method

--- src/parser/instagram.py
+++ src/parser/instagram.py
@@ -1,3 +1,4 @@
+from parser.models import InstagramClientAnswer
 from src.parser.base import BaseThirdPartyAPIClient
 
 
@@ -8,7 +9,7 @@ class InstagramClient(BaseThirdPartyAPIClient):
     api_name = 'InstagramAPI'
     base_url = 'https://www.instagram.com/api/v1/'
 
-    async def get_account_info_by_user_name(self, username: str):
+    async def get_account_info_by_user_name(self, username: str) -> InstagramClientAnswer:
         raw_data = await self.request(
             method=BaseThirdPartyAPIClient.HTTPMethods.GET,
             edge='users/web_profile_info',
@@ -17,5 +18,11 @@ class InstagramClient(BaseThirdPartyAPIClient):
         )
         return raw_data
 
-    async def get_account_stories_by_id(self, id: int):
-        pass
+    async def get_account_stories_by_id(self, user_id: int) -> InstagramClientAnswer:
+        raw_data = await self.request(
+            method=BaseThirdPartyAPIClient.HTTPMethods.GET,
+            edge='feed/reels_media',
+            querystring={'reel_ids': user_id},
+            is_json=True,
+        )
+        return raw_data
