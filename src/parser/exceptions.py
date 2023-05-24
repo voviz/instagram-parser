69d43420a75691d3231915026fbce9f0395c8d6a
glebfel <felyust@list.ru>
2023-05-25 01:36:36 +0300

    add AccountConfirmationRequired, InvalidCredentials

--- src/parser/exceptions.py
+++ src/parser/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class BaseParserException(Exception):
     """Base class for all parser errors"""
 
@@ -10,7 +13,13 @@ class EmptyResultsException(BaseParserException):
 
 
 class ThirdPartyApiException(BaseParserException):
-    pass
+    def __init__(self, api_name: str, status: str, answer: Any):
+        self.api_name = api_name
+        self.status = status
+        self.answer = answer
+
+    def __str__(self):
+        return f'{self.api_name} non-200 response. Status [{self.status}]: {self.answer}'
 
 
 class NotFoundException(BaseParserException):
@@ -23,6 +32,22 @@ class AccountNotExist(ThirdPartyApiException):
 
     def __str__(self):
         return 'Account not exist: {}'.format(self.account_name)
+
+
+class AccountConfirmationRequired(ThirdPartyApiException):
+    def __init__(self, account_name: str):
+        self.account_name = account_name
+
+    def __str__(self):
+        return 'For account {} auth confirmation is required'.format(self.account_name)
+
+
+class InvalidCredentials(ThirdPartyApiException):
+    def __init__(self, account_name: str):
+        self.account_name = account_name
+
+    def __str__(self):
+        return 'Invalid credentials for account {}'.format(self.account_name)
 
 
 class AccountIsPrivate(ThirdPartyApiException):
