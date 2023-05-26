bdbe2cb46520839872766880ad67e430479e1916
glebfel <felyust@list.ru>
2023-05-26 21:58:36 +0300

    update crud methods in InstagramAccountsTableDBHandler

--- src/db/crud/instagram_accounts.py
+++ src/db/crud/instagram_accounts.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 import tortoise
 from tortoise.expressions import Q
 
+from core.settings import settings
 from db.models import InstagramAccounts
 
 
@@ -18,7 +21,29 @@ class InstagramAccountsTableDBHandler:
 
     @classmethod
     async def get_account(cls) -> InstagramAccounts:
-        query = InstagramAccounts.filter(proxy=~Q(proxy=None)).order_by('last_used_at')
-        # update last_used_at field
-        await query.update(last_used_at=tortoise.timezone.now())
-        return await query.first()
+        query = InstagramAccounts.filter(
+            ~Q(proxy=None) & Q(daily_usage_rate__lt=settings.ACCOUNT_DAILY_USAGE_RATE)
+        ).order_by('last_used_at')
+        account = await query.first()
+        if (datetime.now() - account.last_used_at).hour >= 24:
+            # update last_used_at field
+            await query.update(last_used_at=tortoise.timezone.now(),
+                               daily_usage_rate=0)
+        else:
+            # update last_used_at field
+            await query.update(last_used_at=tortoise.timezone.now(),
+                               daily_usage_rate=await cls._get_account_daily_usage_rate(account) + 1)
+        return account
+
+    @classmethod
+    async def _get_account_daily_usage_rate(cls, account: InstagramAccounts) -> int:
+        return (await InstagramAccounts.filter(credentials=account.credentials).first()).daily_usage_rate
+
+    @classmethod
+    async def update_accounts_fully_used(cls) -> None:
+        accounts = await InstagramAccounts.filter(daily_usage_rate__gt=settings.ACCOUNT_DAILY_USAGE_RATE).all()
+        for acc in accounts:
+            if (datetime.now() - acc.last_used_at).hour >= 24:
+                # update last_used_at field
+                await InstagramAccounts.filter(credentials=acc.credentials).update(last_used_at=tortoise.timezone.now(),
+                                                                                   daily_usage_rate=0)
