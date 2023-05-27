d72e0566c335c427841495ffab93eacaf387d764
glebfel <felyust@list.ru>
2023-05-28 01:28:56 +0300

    optimize exceptions

--- src/parser/exceptions.py
+++ src/parser/exceptions.py
@@ -1,15 +1,13 @@
 from typing import Any
 
+from db.models import InstagramAccounts
+
 
 class BaseParserException(Exception):
     """Base class for all parser errors"""
 
     def __str__(self):
         return "Something goes wrong with parser"
-
-
-class EmptyResultsException(BaseParserException):
-    pass
 
 
 class ThirdPartyApiException(BaseParserException):
@@ -22,11 +20,7 @@ class ThirdPartyApiException(BaseParserException):
         return f'{self.api_name} non-200 response. Status [{self.status}]: {self.answer}'
 
 
-class NotFoundException(BaseParserException):
-    pass
-
-
-class AccountNotExist(ThirdPartyApiException):
+class LoginNotExist(ThirdPartyApiException):
     def __init__(self, account_name: str):
         self.account_name = account_name
 
@@ -35,27 +29,19 @@ class AccountNotExist(ThirdPartyApiException):
 
 
 class AccountConfirmationRequired(ThirdPartyApiException):
-    def __init__(self, account_name: str):
-        self.account_name = account_name
+    def __init__(self, account: InstagramAccounts):
+        self.account = account
 
     def __str__(self):
-        return 'For account {} auth confirmation is required'.format(self.account_name)
+        return 'For account {} auth confirmation is required'.format(self.account.credentials)
 
 
-class InvalidCredentials(ThirdPartyApiException):
-    def __init__(self, account_name: str):
-        self.account_name = account_name
+class AccountInvalidCredentials(ThirdPartyApiException):
+    def __init__(self, account: InstagramAccounts):
+        self.account = account
 
     def __str__(self):
-        return 'Invalid credentials for account: {}'.format(self.account_name)
-
-
-class AccountIsPrivate(ThirdPartyApiException):
-    pass
-
-
-class ThirdPartyTimeoutError(ThirdPartyApiException):
-    pass
+        return 'Invalid credentials for account: {}'.format(self.account.credentials)
 
 
 class InvalidProxyFormatError(BaseParserException):
