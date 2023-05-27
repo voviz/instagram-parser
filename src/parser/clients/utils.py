173dab9e08ab362ff2f01187866196cc003cacc8
glebfel <felyust@list.ru>
2023-05-28 01:28:11 +0300

    add errors_handler_decorator for errors handling

---
+++ src/parser/clients/utils.py
@@ -0,0 +1,45 @@
+import asyncio
+
+import aiohttp
+from selenium.common import WebDriverException
+
+from core.logs import custom_logger
+from db.connector import DatabaseConnector
+from db.crud.instagram_accounts import InstagramAccountsTableDBHandler
+from db.crud.instagram_logins import InstagramLoginsTableDBHandler
+from parser.exceptions import AccountConfirmationRequired, AccountInvalidCredentials, LoginNotExist
+
+
+def errors_handler_decorator(func):
+    async def wrapper(*args, **kwargs):
+        try:
+            # init db
+            await DatabaseConnector().async_init()
+            res = await func(*args, **kwargs)
+            return res
+        except (asyncio.TimeoutError,
+                aiohttp.ClientOSError,
+                aiohttp.ClientResponseError,
+                aiohttp.ServerDisconnectedError,
+                aiohttp.client_exceptions.ClientProxyConnectionError,
+                ConnectionResetError,
+                ConnectionError,
+                ConnectionAbortedError) as ex:
+            custom_logger.error(f'Connection error ({type(ex)}): {ex}')
+        except (AccountInvalidCredentials, AccountConfirmationRequired) as ex:
+            # delete acc from db
+            await InstagramAccountsTableDBHandler.delete_account(ex.account)
+            custom_logger.error(ex)
+        except LoginNotExist as ex:
+            # mark login as not existed
+            await InstagramLoginsTableDBHandler.mark_as_not_exists(ex.account_name)
+            custom_logger.error(ex)
+        except WebDriverException as ex:
+            custom_logger.error(f'Error with story link resolving process ({type(ex)}): {ex}')
+        except Exception as ex:
+            custom_logger.error(f'Something wrong with parser ({type(ex)}): {ex}')
+        finally:
+            # close db connection
+            await DatabaseConnector().close()
+
+    return wrapper
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -2,6 +2,8 @@ import asyncio
 import concurrent.futures
 import random
 
+import tortoise
+
 from core.logs import custom_logger
 from core.settings import settings
 from db.connector import DatabaseConnector
@@ -10,31 +12,30 @@ from db.crud.instagram_logins import InstagramLoginsTableDBHandler
 from db.crud.proxies import ProxiesTableDBHandler
 from db.models import InstagramLogins
 from parser.clients.instagram import InstagramClient
+from parser.clients.utils import errors_handler_decorator
 
 
 class Parser:
+    @errors_handler_decorator
     async def on_start(self):
         custom_logger.info('Start parser ...')
         custom_logger.info('Prepare database ...')
-        # init db
-        await DatabaseConnector().async_init()
         # get new accs and union with proxies
         new_accounts = await InstagramAccountsTableDBHandler.get_accounts_without_proxy()
         proxies = await ProxiesTableDBHandler.get_proxies_all()
-        res = []
         for i, acc in enumerate(new_accounts):
-            res.append((acc, proxies[i % len(proxies)]))
-        await InstagramAccountsTableDBHandler.set_proxy_for_accounts(res)
-
+            acc.proxy = proxies[i % len(proxies)].proxy
+        await InstagramAccountsTableDBHandler.set_proxy_for_accounts(new_accounts)
         # update usage rates for all accs
         await InstagramAccountsTableDBHandler.update_accounts_daily_usage_rate()
         custom_logger.info('Parser is ready ...')
-
         # return logins for update
         logins_for_update = await InstagramLoginsTableDBHandler.get_login_all()
         custom_logger.info(f'{len(logins_for_update)} logins for update found!')
+
         return logins_for_update
 
+    @errors_handler_decorator
     async def collect_instagram_story_data(self, login: InstagramLogins) -> None:
         client = InstagramClient()
         if not login.user_id:
@@ -55,7 +56,7 @@ class Parser:
         # on_start run
         logins_for_update = asyncio.run(self.on_start())
         futures = []
-        with concurrent.futures.ProcessPoolExecutor(max_workers=settings.PROCESS_COUNT) as executor:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
             for login in logins_for_update:
                 new_future = executor.submit(
                     self.sync_wrapper,
