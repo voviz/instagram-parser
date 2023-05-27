7c208fa18a45f8886123d2dc8ebfaf4141845386
glebfel <felyust@list.ru>
2023-05-27 16:48:12 +0300

    made methods in DatabaseConnector class as class-methods

--- src/db/connector.py
+++ src/db/connector.py
@@ -9,10 +9,14 @@ log = logging.getLogger(__name__)
 
 
 class DatabaseConnector:
-    def __init__(self):
-        self._sync_init()
+    @classmethod
+    def sync_init(cls):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(cls.async_init())
 
-    async def _async_init(self):
+    @classmethod
+    async def async_init(cls):
         await Tortoise.init(
             db_url=f'asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@'
                    f'{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}',
@@ -21,11 +25,3 @@ class DatabaseConnector:
         # Generate the schema
         # safe=True - generate schema if not exists in db
         await Tortoise.generate_schemas(safe=True)
-
-    def _sync_init(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(self._async_init())
-
-
-database_connector = DatabaseConnector()
