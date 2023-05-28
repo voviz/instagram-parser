ef816f2c1f3a4158b44b661fd03298b7c34670d1
glebfel <felyust@list.ru>
2023-05-28 19:30:18 +0300

    fix update_result method

--- src/db/crud/parser_result.py
+++ src/db/crud/parser_result.py
@@ -7,17 +7,15 @@ class ParserResultTableDBHandler:
     async def update_result(cls, login: InstagramClientAnswer) -> None:
         if login.stories_list:
             for story in login.stories_list:
-                if story.sku:
-                    await ParserResult.update_or_create(instagram_username=login.username,
-                                                        marketplace=story.marketplace,
-                                                        story_publication_date=story.created_at,
-                                                        sku=story.sku,
-                                                        ad_type=story.ad_type,
-                                                        defaults={'marketplace': story.marketplace,
-                                                                  'story_publication_date': story.created_at,
-                                                                  'sku': story.sku,
-                                                                  'ad_type': story.ad_type, })
-                    break
+                await ParserResult.update_or_create(instagram_username=login.username,
+                                                    marketplace=story.marketplace.value,
+                                                    story_publication_date=story.created_at,
+                                                    sku=story.sku,
+                                                    ad_type=story.ad_type.value,
+                                                    defaults={'marketplace': story.marketplace.value,
+                                                              'story_publication_date': story.created_at,
+                                                              'sku': story.sku,
+                                                              'ad_type': story.ad_type.value, })
 
     @classmethod
     async def get_result_by_username(cls, username: str) -> ParserResult:
