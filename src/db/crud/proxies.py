7cb4461520e01e296966c4a7af075e2591c4588c
glebfel <felyust@list.ru>
2023-05-28 16:15:45 +0300

    add ProxyTypes

--- src/db/crud/proxies.py
+++ src/db/crud/proxies.py
@@ -1,11 +1,18 @@
+from enum import StrEnum
+
 from db.models import Proxies
+
+
+class ProxyTypes(StrEnum):
+    ozon = 'ozon'
+    parser = 'parser'
 
 
 class ProxiesTableDBHandler:
     @classmethod
     async def get_parser_proxies_all(cls) -> list[Proxies]:
-        return await Proxies.filter(type='parser').all()
+        return await Proxies.filter(type=ProxyTypes.parser.value).all()
 
     @classmethod
     async def get_ozon_proxy(cls) -> Proxies:
-        return await Proxies.filter(type='ozon').first()
+        return await Proxies.filter(type=ProxyTypes.ozon.value).first()
