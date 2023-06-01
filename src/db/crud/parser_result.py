9958bbf607a05bebbed5ebc459b659a8f96c67aa
glebfel <felyust@list.ru>
2023-06-01 19:08:21 +0300

    add bulk update method for results table db

--- src/db/crud/parser_result.py
+++ src/db/crud/parser_result.py
@@ -11,7 +11,21 @@ class ParserResultTableDBHandler:
                                           marketplace=story.marketplace.value,
                                           story_publication_date=story.created_at,
                                           sku=story.sku,
-                                          ad_type=story.ad_type.value,)
+                                          ad_type=story.ad_type.value, )
+
+    @classmethod
+    async def add_result_list(cls, login_list: list[InstagramClientAnswer]) -> None:
+        result_list = []
+        for login in login_list:
+            if login.stories_list:
+                for story in login.stories_list:
+                    result_list.append(ParserResult(instagram_username=login.username,
+                                                    marketplace=story.marketplace.value,
+                                                    story_publication_date=story.created_at,
+                                                    sku=story.sku,
+                                                    ad_type=story.ad_type.value, ))
+
+        await ParserResult.bulk_create(result_list)
 
     @classmethod
     async def get_result_by_username(cls, username: str) -> ParserResult:
