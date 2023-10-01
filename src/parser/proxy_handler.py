138834ef900f438f964a02105dbbb289f013782c
glebfel <felyust@list.ru>
2023-10-01 16:07:55 +0300

    fix proxy handler (add ip:port:login:password format support)

--- src/parser/proxy_handler.py
+++ src/parser/proxy_handler.py
@@ -4,33 +4,57 @@ import tempfile
 from src.parser.exceptions import InvalidProxyFormatError
 
 
-class ProxyHandler:
-    @staticmethod
-    def _split_proxy(proxy: str) -> list:
-        proxy = proxy.lstrip('http://')
-        proxy_parts = proxy.split('@') if '@' in proxy else [proxy]
-        parts = []
-        for p in proxy_parts:
-            parts.extend(p.split(':'))
-        if len(parts) < 4:
+def _split_proxy(proxy: str) -> tuple:
+    proxy = proxy.lstrip('http://')  # noqa: B005
+
+    def is_ip(part: str) -> bool:
+        return '.' in part and part.replace('.', '').isdigit()
+
+    parts = proxy.split(':')
+
+    # Если формат 'ip:port:login:password'
+    if len(parts) == 4 and '@' not in proxy and is_ip(parts[0]):
+        ip, port, login, password = parts
+
+    # Если формат 'login:password:ip:port'
+    elif len(parts) == 4 and '@' not in proxy:
+        login, password, ip, port = parts
+
+    else:
+        first_parts, _, last_parts = proxy.partition('@')
+
+        if not first_parts or not last_parts:
             raise InvalidProxyFormatError(proxy)
-        return parts
 
-    @staticmethod
-    def convert_to_aiohttp_format(proxy: str) -> str:
-        """
-        convert to format -> http://login:password@ip:port
-        """
-        login, password, ip, port = ProxyHandler._split_proxy(proxy)
-        return f'http://{login}:{password}@{ip}:{port}'
+        # Если формат 'ip:port@login:password'
+        if is_ip(first_parts):
+            ip, _, port = first_parts.partition(':')
+            login, _, password = last_parts.partition(':')
+        # Если формат 'login:password@ip:port'
+        else:
+            login, _, password = first_parts.partition(':')
+            ip, _, port = last_parts.partition(':')
 
-    @staticmethod
-    def convert_to_seleniumbase_format(proxy: str) -> str:
-        """
-        convert to format -> login:password@ip:port
-        """
-        login, password, ip, port = ProxyHandler._split_proxy(proxy)
-        return f'{login}:{password}@{ip}:{port}'
+    if not ip or not port.isdigit() or not login or not password:
+        raise InvalidProxyFormatError(proxy)
+
+    return login, password, ip, port
+
+
+def convert_to_aiohttp_format(proxy: str) -> str:
+    """
+    convert to format -> http://login:password@ip:port
+    """
+    login, password, ip, port = _split_proxy(proxy)
+    return f'http://{login}:{password}@{ip}:{port}'
+
+
+def convert_to_seleniumbase_format(proxy: str) -> str:
+    """
+    convert to format -> login:password@ip:port
+    """
+    login, password, ip, port = _split_proxy(proxy)
+    return f'{login}:{password}@{ip}:{port}'
 
 
 class SeleniumProxyHandler:
@@ -112,5 +136,5 @@ class SeleniumProxyHandler:
         """
         from format like ip:port:login:password -> http://login:password@ip:port
         """
-        ip, port, login, password = ProxyHandler._split_proxy(proxy)
+        ip, port, login, password = _split_proxy(proxy)
         return ip, int(port), login, password
