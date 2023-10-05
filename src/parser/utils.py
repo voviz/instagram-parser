9ed26b667da222904c21498bed4562a9cbcee8d1
glebfel <felyust@list.ru>
2023-10-06 02:16:54 +0300

    add logging for check_driver_installation method

--- src/parser/utils.py
+++ src/parser/utils.py
@@ -1,10 +1,14 @@
 from seleniumbase import get_driver
 
+from src.core.logs import custom_logger
+
 
 def check_driver_installation() -> None:
+    custom_logger.info('Check webdriver installation ... ')
     driver = get_driver('chrome', headless=True)
     driver.get('https://www.google.com/chrome')
     driver.quit()
+    custom_logger.info('End of check driver installation process ...')
 
 
 def chunks(lst: list, n: int):
