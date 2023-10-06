de78a93392f80a717279626d5030767735e42887
glebfel <felyust@list.ru>
2023-10-06 14:39:59 +0300

    move errors_handler_decorator to parser level utils module

--- src/parser/utils.py
+++ src/parser/utils.py
@@ -1,6 +1,23 @@
+import asyncio
+import random
+
+import aiohttp
+from selenium.common import TimeoutException, WebDriverException
 from seleniumbase import get_driver
 
 from src.core.logs import custom_logger
+from src.db.connector import async_session
+from src.db.crud.instagram_accounts import delete_account
+from src.db.crud.instagram_logins import mark_as_not_exists
+from src.parser.exceptions import (
+    AccountConfirmationRequired,
+    AccountInvalidCredentials,
+    AccountTooManyRequests,
+    LoginNotExist,
+    NoProxyDBError,
+    ProxyTooManyRequests,
+    ThirdPartyApiException,
+)
 
 
 def check_driver_installation() -> None:
@@ -20,3 +37,59 @@ def chunks(lst: list, n: int):
     """
     for i in range(0, len(lst), n):
         yield lst[i: i + n]
+
+
+def errors_handler_decorator(func):  # noqa: CCR001
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except (
+                asyncio.TimeoutError,
+                aiohttp.ClientOSError,
+                aiohttp.ClientResponseError,
+                aiohttp.ServerDisconnectedError,
+                aiohttp.client_exceptions.ClientProxyConnectionError,
+                ConnectionError,
+        ) as ex:
+            custom_logger.error(f'Connection error ({type(ex)}): {ex}')
+            await asyncio.sleep(random.randint(2, 5))
+        except (aiohttp.ClientProxyConnectionError, aiohttp.ClientHttpProxyError) as ex:
+            custom_logger.error(f'Cannot connect via proxy: {ex.proxy}')
+            await asyncio.sleep(random.randint(2, 5))
+        except (AccountInvalidCredentials, AccountConfirmationRequired, AccountTooManyRequests) as ex:
+            await account_errors(ex)
+        except LoginNotExist as ex:
+            await login_errors(ex)
+        except ProxyTooManyRequests as ex:
+            custom_logger.warning(ex)
+        except ThirdPartyApiException as ex:
+            custom_logger.error(ex)
+        except NoProxyDBError as ex:
+            await no_proxy_db_error(ex)
+        except TimeoutException as ex:
+            custom_logger.error(f'Error with story link resolving process ({type(ex)}) url: {ex.url}')
+        except WebDriverException as ex:
+            custom_logger.error(f'Error with webdriver in story link resolving process ({type(ex)}): {ex}')
+            custom_logger.error('Maybe something occur with "ozon" proxy....')
+        except Exception as ex:  # noqa: PIE786
+            custom_logger.error(f'Something wrong with parser ({type(ex)}): {ex}')
+
+    return wrapper
+
+
+async def account_errors(ex):
+    custom_logger.warning(ex)
+    async with async_session() as s:
+        await delete_account(s, ex.account)
+
+
+async def login_errors(ex):
+    async with async_session() as s:
+        await mark_as_not_exists(s, ex.account_name)
+    custom_logger.warning(ex)
+
+
+async def no_proxy_db_error(ex):
+    custom_logger.warning(ex)
+    custom_logger.warning('Restart after 15 min ...')
+    await asyncio.sleep(900)
